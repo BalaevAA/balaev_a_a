d824d8e06361ba8006d7be02835ea51cf7e016ab
Anton <balaevantoine@gmaiil.com>
2022-02-14 14:49:56 +0300

    lab_01 and part of lab_02

--- prj.labs/lab02/lab02.cpp
+++ prj.labs/lab02/lab02.cpp
@@ -1,4 +1,27 @@
 #include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <string>
 
 int main() {
+	//1
+	std::string img_path = "C:/Users/balae/Documents/balaev_a_a/data/cross_0256x0256.png";
+	cv::Mat img = cv::imread(img_path);
+	if (img.empty()) {
+		std::cout << "Could not read the image: " << img_path << std::endl;
+		return 1;
+	}
+	//2
+	cv::imwrite("cross_0256x0256_025.jpeg", img, { cv::IMWRITE_JPEG_QUALITY , 25 });
+	//3
+
+
+
+
+
+
+
+
+	cv::waitKey(0);
+
+	return 0;
 }
