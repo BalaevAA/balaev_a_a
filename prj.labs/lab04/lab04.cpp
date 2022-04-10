13c6a43209d33f5622e5b02d3ef42d85262703b1
Anton <balaevantoine@gmaiil.com>
2022-04-11 00:30:54 +0300

    lab04

--- prj.labs/lab04/lab04.cpp
+++ prj.labs/lab04/lab04.cpp
@@ -3,16 +3,13 @@
 #include <iostream>
 #include <vector>
 
-
-
 std::vector<cv::Mat> selectFramesFromVideo(std::string path, std::string name);
 std::vector<cv::Mat> colorConversation(std::vector<cv::Mat> f, std::string name);
 std::vector<cv::Mat> tresholdForEveryone(std::vector<cv::Mat> f, std::string name);
 std::vector<cv::Mat> morph(std::vector<cv::Mat> f, std::string name);
-std::vector<cv::Mat> connnectedComponents(std::vector<cv::Mat> f, std::string name);
+std::vector<cv::Mat> connnectedComponents(std::vector<cv::Mat> f, std::vector<std::vector<std::pair<int, int> > > p, std::string name);
 void makeMosaic(std::vector<cv::Mat> src, std::string name, int type);
-
-
+double sq(const std::vector<std::pair<int, int> >& fig);
 
 std::vector<cv::Mat> selectFramesFromVideo(std::string path, std::string name) {
 
@@ -40,10 +37,8 @@ std::vector<cv::Mat> selectFramesFromVideo(std::string path, std::string name) {
 	return res;
 }
 
-
 std::vector<cv::Mat> colorConversation(std::vector<cv::Mat> f, std::string name) {
 	std::vector<cv::Mat> vec;
-	std::cout << "\n\n" << f[0].size() << "\n\n";
 	for (int i = 0; i < f.size(); ++i) {
 		cv::Mat tmp(640, 360, CV_8UC3);
 		cv::cvtColor(f[i], tmp, cv::COLOR_BGR2GRAY);
@@ -59,6 +54,10 @@ std::vector<cv::Mat> tresholdForEveryone(std::vector<cv::Mat> f, std::string nam
 	for (int i = 0; i < f.size(); ++i) {
 		cv::Mat tmp(640, 360, CV_8UC3);
 		cv::threshold(f[i], tmp, 0, max_binary_value, cv::THRESH_BINARY | cv::THRESH_OTSU);
+		//cv::threshold(f[i], tmp1, 0, 255, cv::THRESH_BINARY + cv::THRESH_OTSU);
+		//cv::GaussianBlur(f[i], blurred, cv::Size(5, 5), 0);
+		//cv::threshold(blurred, tmp2, 0, 255, cv::THRESH_BINARY + cv::THRESH_OTSU);
+		//cv::adaptiveThreshold(f[i], tmp, max_binary_value, 0, 2, 25, 10);
 		cv::imwrite(name + "treshold_" + std::to_string(i) + ".png", tmp);
 		vec.push_back(tmp);
 	}
@@ -67,11 +66,9 @@ std::vector<cv::Mat> tresholdForEveryone(std::vector<cv::Mat> f, std::string nam
 
 std::vector<cv::Mat> morph(std::vector<cv::Mat> f, std::string name) {
 	std::vector<cv::Mat> vec;
-
-
 	for (int i = 0; i < f.size(); ++i) {
 		cv::Mat tmp(640, 360, CV_8UC3);
-		cv::Mat k = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3), cv::Point(-1, -1));
+		cv::Mat k = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(6, 6), cv::Point(-1, -1));
 		cv::morphologyEx(f[i], tmp, cv::MORPH_OPEN, k);
 		cv::morphologyEx(f[i], tmp, cv::MORPH_CLOSE, k);
 		vec.push_back(tmp);
@@ -81,18 +78,18 @@ std::vector<cv::Mat> morph(std::vector<cv::Mat> f, std::string name) {
 	return vec;
 }
 
-std::vector<cv::Mat> connnectedComponents(std::vector<cv::Mat> f, std::string name) {
+std::vector<cv::Mat> connnectedComponents(std::vector<cv::Mat> f, std::vector<std::vector<std::pair<int, int> > > p, std::string name) {
 	std::vector<cv::Mat> vec;
 	for (int i = 0; i < f.size(); ++i) {
+		int max_area = -1, x_max = -1, y_max = -1, w_max = -1, h_max = -1;
 		cv::Mat labels;
 		cv::Mat stats, centroids;
 		int num_labels = cv::connectedComponentsWithStats(f[i], labels, stats, centroids, 8, 4);
 
-
 		std::vector<cv::Vec3b> colors(num_labels);
 
 		for (int i = 0; i < num_labels; ++i) {
-			colors[i] = cv::Vec3b(0, 0, 0);
+			colors[i] = cv::Vec3b(255, 255, 255);
 		}
 
 		cv::Mat dst = cv::Mat::zeros(f[i].size(), CV_8UC3);
@@ -107,17 +104,27 @@ std::vector<cv::Mat> connnectedComponents(std::vector<cv::Mat> f, std::string na
 			}
 		}
 
-		for (int i = 1; i < num_labels; ++i) {
-			cv::Vec2d pt = centroids.at<cv::Vec2d>(i, 0);
-			int x = stats.at<int>(i, cv::CC_STAT_LEFT);
-			int y = stats.at<int>(i, cv::CC_STAT_TOP);
-			int width = stats.at<int>(i, cv::CC_STAT_WIDTH);
-			int heigth = stats.at<int>(i, cv::CC_STAT_HEIGHT);
-			int area = stats.at<int>(i, cv::CC_STAT_AREA);
-			std::cout << "area: " << area << " center point " << pt[0] << " " << pt[1] << "\n";
-			cv::circle(dst, cv::Point(pt[0], pt[1]), 2, cv::Scalar(0, 0, 255), -1, 8, 0);
-			cv::rectangle(dst, cv::Rect(x, y, width, heigth), cv::Scalar(255, 0, 255), 1, 8, 0);
+		for (int j = 1; j < num_labels; ++j) {
+			cv::Vec2d pt = centroids.at<cv::Vec2d>(j, 0);
+			int x = stats.at<int>(j, cv::CC_STAT_LEFT);
+			int y = stats.at<int>(j, cv::CC_STAT_TOP);
+			int width = stats.at<int>(j, cv::CC_STAT_WIDTH);
+			int heigth = stats.at<int>(j, cv::CC_STAT_HEIGHT);
+			int area = stats.at<int>(j, cv::CC_STAT_AREA);
+			if (area >= max_area) {
+				max_area = area;
+				x_max = x;
+				y_max = y;
+				w_max = width;
+				h_max = heigth;
+			}
+			//cv::rectangle(dst, cv::Rect(x, y, width, heigth), cv::Scalar(255, 0, 255), 1, 8, 0);
 		}
+		//std::cout << "(x,y) = (" << x_max << ", " << y_max << ") and (width, height) = (" << w_max << ", " << h_max << ")\n";
+		//std::cout << "area: " << max_area << "\n";
+		std::cout << name << "\n";
+		std::cout << "accuracy(square_orig / square_conn_comp): " << sq(p[i]) / max_area << "\n\n\n";
+		cv::rectangle(dst, cv::Rect(x_max, y_max, w_max, h_max), cv::Scalar(255, 0, 255), 1, 8, 0);
 		std::cout << "\n";
 		vec.push_back(dst);
 		cv::imwrite(name + "con" + std::to_string(i) + ".png", dst);
@@ -125,7 +132,7 @@ std::vector<cv::Mat> connnectedComponents(std::vector<cv::Mat> f, std::string na
 	return vec;
 }
 
- void makeMosaic(std::vector<cv::Mat> src, std::string name, int type) {
+void makeMosaic(std::vector<cv::Mat> src, std::string name, int type) {
 	cv::Mat res_img(720, 1280, type);
 	src[0].copyTo(res_img(cv::Rect(0, 0, 640, 360)));
 	src[1].copyTo(res_img(cv::Rect(640, 360, 640, 360)));
@@ -136,6 +143,15 @@ std::vector<cv::Mat> connnectedComponents(std::vector<cv::Mat> f, std::string na
 	//return res_img;
 }
 
+double sq(const std::vector<std::pair<int, int> >& fig){
+	double res = 0;
+	for (unsigned i = 0; i < fig.size(); i++) {
+		std::pair<int, int> p1 = i ? fig[i - 1] : fig.back(),
+																			p2 = fig[i];
+		 res += (p1.first - p2.first) * (p1.second + p2.second);
+	 }
+	 return fabs(res) / 2;
+ }
 
 int main() {
 
@@ -145,9 +161,13 @@ int main() {
 		morph_frames,
 		connected_comp;
 
-	cv::Mat mosaic_original, mosaic_gray, mosaic_bin, mosaic_morph, mosaic_comp;
-	///////////////////////////////////////////  1  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-		
+
+	//////////////////  1  \\\\\\\\\\\\\\\\\\\\\
+	
+	std::vector<std::vector<std::pair<int, int> > > p1 = { { {136, 71}, {589, 90}, {587, 283}, {131, 277}},
+																												 { {153, 56}, {589, 75}, {587, 259}, {149, 255} },
+			  																								 { {101, 59}, {555, 71}, {553, 263}, {97, 264} } };
+
 	frames = selectFramesFromVideo("../data/rub100(2).mp4", "lab04_rub100_2");
 	makeMosaic(frames, "rub100_2", CV_8UC3);
 
@@ -160,29 +180,58 @@ int main() {
 	morph_frames = morph(bin_frames, "lab04_rub100_2");
 	makeMosaic(morph_frames, "morph_rub100_2", CV_32S);
 
-	connected_comp = connnectedComponents(morph_frames, "lab04_rub100_2");
+	connected_comp = connnectedComponents(morph_frames, p1, "lab04_rub100_2");
 	makeMosaic(connected_comp, "comp_rub100_2", CV_8UC3);
 	
+	///////////////////  2  \\\\\\\\\\\\\\\\\\\\\
+
+	std::vector<std::vector<std::pair<int, int> > > p2 = { { {133,108}, {490,110}, {493,264}, {133,265} },
+																												 { {129,114}, {491,117}, {493,272}, {129,273} },
+																												 { {114,115}, {470,118}, {472,269}, {113,272} } };
+
+	frames = selectFramesFromVideo("../data/rub50.mp4", "lab04_rub50");
+	makeMosaic(frames, "rub50", CV_8UC3);
+
+	gray_frames = colorConversation(frames, "lab04_rub50");
+	makeMosaic(gray_frames, "gray_rub50", CV_32S);
+
+	bin_frames = tresholdForEveryone(gray_frames, "lab04_rub50");
+	makeMosaic(bin_frames, "bin_rub50", CV_32S);
+
+	morph_frames = morph(bin_frames, "lab04_rub50");
+	makeMosaic(morph_frames, "morph_rub50", CV_32S);
+
+	connected_comp = connnectedComponents(morph_frames, p2, "lab04_rub50");
+	makeMosaic(connected_comp, "comp_rub50", CV_8UC3);
+
+
+	//////////////////  3  \\\\\\\\\\\\\\\\\\\\\
+
+	std::vector<std::vector<std::pair<int, int> > > p3 = { { {129, 97}, {474, 102}, {473, 252}, {124, 247} },
+																										     { {141, 109}, {490, 115}, {489, 266}, {134, 261} },
+																										     { {141, 119}, {493, 127}, {493, 278}, {135, 273} } };
+
+	frames = selectFramesFromVideo("../data/rub200(4).mp4", "lab04_rub200_4");
+	makeMosaic(frames, "rub200_4", CV_8UC3);
+
+	gray_frames = colorConversation(frames, "lab04_rub200_4");
+	makeMosaic(gray_frames, "gray_rub200_4", CV_32S);
+
+	bin_frames = tresholdForEveryone(gray_frames, "lab04_rub200_4");
+	makeMosaic(bin_frames, "bin_rub200_4", CV_32S);
+
+	morph_frames = morph(bin_frames, "lab04_rub200_4");
+	makeMosaic(morph_frames, "morph_rub200_4", CV_32S);
+
+	connected_comp = connnectedComponents(morph_frames, p3, "lab04_rub200_4");
+	makeMosaic(connected_comp, "comp_rub200_4", CV_8UC3);
 	
-	///////////////////////////////////////////  2  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-	frames = selectFramesFromVideo("../data/rub200(3).mp4", "lab04_rub200_3");
-	makeMosaic(frames, "rub200_3", CV_8UC3);
+	//////////////////  4  \\\\\\\\\\\\\\\\\\\\\
 
-	gray_frames = colorConversation(frames, "lab04_rub200_3");
-	makeMosaic(gray_frames, "gray_rub200_3", CV_32S);
-
-	bin_frames = tresholdForEveryone(gray_frames, "lab04_rub200_3");
-	makeMosaic(bin_frames, "bin_rub200_3", CV_32S);
-
-	morph_frames = morph(bin_frames, "lab04_rub200_3");
-	makeMosaic(morph_frames, "morph_rub200_3", CV_32S);
-
-	connected_comp = connnectedComponents(morph_frames, "lab04_rub200_3");
-	makeMosaic(connected_comp, "comp_rub200_3", CV_8UC3);
-
-
-	///////////////////////////////////////////  3  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	std::vector<std::vector<std::pair<int, int> > > p4 = { { {133, 87}, {501, 86}, {498, 243}, {133, 244} },
+																												 { {113, 81}, {481, 81}, {479, 233}, {114, 239} },
+																												 { {115, 76}, {487, 78}, {484, 236}, {114, 235} } };
 
 	frames = selectFramesFromVideo("../data/rub100(3).mp4", "lab04_rub100_3");
 	makeMosaic(frames, "rub100_3", CV_8UC3);
@@ -196,29 +245,15 @@ int main() {
 	morph_frames = morph(bin_frames, "lab04_rub100_3");
 	makeMosaic(morph_frames, "morph_rub100_3", CV_32S);
 
-	connected_comp = connnectedComponents(morph_frames, "lab04_rub100_3");
+	connected_comp = connnectedComponents(morph_frames, p4, "lab04_rub100_3");
 	makeMosaic(connected_comp, "comp_rub100_3", CV_8UC3);
 
 
-	///////////////////////////////////////////  4  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	//////////////////  5  \\\\\\\\\\\\\\\\\\\\\
 
-	frames = selectFramesFromVideo("../data/rub500.mp4", "lab04_rub500");
-	makeMosaic(frames, "rub500", CV_8UC3);
-
-	gray_frames = colorConversation(frames, "lab04_rub500");
-	makeMosaic(gray_frames, "gray_rub500", CV_32S);
-
-	bin_frames = tresholdForEveryone(gray_frames, "lab04_rub500");
-	makeMosaic(bin_frames, "bin_rub500", CV_32S);
-
-	morph_frames = morph(bin_frames, "lab04_rub500");
-	makeMosaic(morph_frames, "morph_rub500", CV_32S);
-
-	connected_comp = connnectedComponents(morph_frames, "lab04_rub500");
-	makeMosaic(connected_comp, "comp_rub500", CV_8UC3);
-
-
-	///////////////////////////////////////////  5  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	std::vector<std::vector<std::pair<int, int> > > p5 = { { {99, 67}, {565, 83}, {565, 281}, {97, 275} },
+																												 { {91, 59}, {583, 75}, {584, 285}, {88, 280} },
+																												 { {100, 53}, {578, 69}, {581, 273}, {97, 269} } };
 
 	frames = selectFramesFromVideo("../data/rub500(2).mp4", "lab04_rub500_2");
 	makeMosaic(frames, "rub500_2", CV_8UC3);
@@ -232,10 +267,9 @@ int main() {
 	morph_frames = morph(bin_frames, "lab04_rub500_2");
 	makeMosaic(morph_frames, "morph_rub500_2", CV_32S);
 
-	connected_comp = connnectedComponents(morph_frames, "lab04_rub500_2");
+	connected_comp = connnectedComponents(morph_frames, p5, "lab04_rub500_2");
 	makeMosaic(connected_comp, "comp_rub500_2", CV_8UC3);
 	
-
 	cv::waitKey(0);
 	return 0;
 }
--- prj.labs/test/test.cpp
+++ prj.labs/test/test.cpp
@@ -1,69 +1,175 @@
 #include <opencv2/opencv.hpp>
 #include <string>
 #include <iostream>
+#include <vector>
+
+void doMagic(std::string path, std::string name, std::vector<std::vector<std::pair<int, int> > > coord);
+void makeMosaic(std::vector<cv::Mat> src, std::string name, int type);
+double sq(const std::vector<std::pair<int, int> >& fig);
 
 
+void doMagic(std::string path, std::string name, std::vector<std::vector<std::pair<int, int> > > coord) {
+	cv::VideoCapture cp(path);
+	std::vector<cv::Mat> original_images(3);
 
-void connected_component_stats_demo(cv::Mat& image);
+	int amount_of_frames = cp.get(cv::CAP_PROP_FRAME_COUNT);
+	int f1 = (amount_of_frames / 5) * 2,
+		f2 = (amount_of_frames / 5) * 3,
+		f3 = (amount_of_frames / 5) * 4;
+	cv::Mat tmp1, tmp2, tmp3;
+	cp.set(cv::CAP_PROP_POS_FRAMES, f1);
+	cp >> original_images[0];
+	cv::resize(original_images[0], original_images[0], cv::Size(640, 360), cv::INTER_LINEAR);
+	cp.set(cv::CAP_PROP_POS_FRAMES, f2);
+	cp >> original_images[1];
+	cv::resize(original_images[1], original_images[1], cv::Size(640, 360), cv::INTER_LINEAR);
+	cp.set(cv::CAP_PROP_POS_FRAMES, f3);
+	cp >> original_images[2];
+	cv::resize(original_images[2], original_images[2], cv::Size(640, 360), cv::INTER_LINEAR);
 
-int main() {
-	cv::Mat src = cv::imread("C:/Users/balae/Documents/balaev_a_a/bin.dbg/lab04_rub200_2_img3.png");
-	if (src.empty()) {
-		std::cout << "couldn't load image";
+	cv::imwrite(name + "_img1.png", original_images[0]);
+	cv::imwrite(name + "_img2.png", original_images[1]);
+	cv::imwrite(name + "_img3.png", original_images[2]);
+	makeMosaic(original_images, name, CV_8UC3);
+
+	std::vector<cv::Mat> gray_images;
+	for (int i = 0; i < original_images.size(); ++i) {
+		cv::Mat tmp(640, 360, CV_8UC3);
+		cv::cvtColor(original_images[i], tmp, cv::COLOR_BGR2GRAY);
+		cv::imwrite(name + "binary_" + std::to_string(i) + ".png", tmp);
+		gray_images.push_back(tmp);
 	}
-	cv::imshow("imshow", src);
-	connected_component_stats_demo(src);
-	cv::waitKey(0);
-	return 0;
+	makeMosaic(gray_images, "gray_" + name, CV_32S);
+
+	int const max_binary_value = 255;
+	std::vector<cv::Mat> bin_images;
+	for (int i = 0; i < gray_images.size(); ++i) {
+		cv::Mat tmp(640, 360, CV_8UC3);
+		cv::threshold(gray_images[i], tmp, 0, max_binary_value, cv::THRESH_BINARY | cv::THRESH_OTSU);
+		cv::imwrite(name + "treshold_" + std::to_string(i) + ".png", tmp);
+		bin_images.push_back(tmp);
+	}
+	makeMosaic(bin_images, "bin_" + name, CV_32S);
+
+	std::vector<cv::Mat> morph_images;
+	for (int i = 0; i < bin_images.size(); ++i) {
+		cv::Mat tmp(640, 360, CV_8UC3);
+		cv::Mat k = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3), cv::Point(-1, -1));
+		cv::morphologyEx(bin_images[i], tmp, cv::MORPH_OPEN, k);
+		//cv::morphologyEx(f[i], tmp, cv::MORPH_CLOSE, k);
+		morph_images.push_back(tmp);
+		cv::imwrite(name + "_morph_" + std::to_string(i) + ".png", tmp);
+	}
+	makeMosaic(morph_images, "morph_" + name, CV_32S);
+
+	std::vector<cv::Mat> conn_comp;
+	for (int i = 0; i < morph_images.size(); ++i) {
+		int max_area = -1, x_max = -1, y_max = -1, w_max = -1, h_max = -1;
+		cv::Mat labels;
+		cv::Mat stats, centroids;
+		int num_labels = cv::connectedComponentsWithStats(morph_images[i], labels, stats, centroids, 8, 4);
+
+		std::vector<cv::Vec3b> colors(num_labels);
+
+		for (int i = 0; i < num_labels; ++i) {
+			colors[i] = cv::Vec3b(255, 255, 255);
+		}
+
+		cv::Mat dst = cv::Mat::zeros(morph_images[i].size(), CV_8UC3);
+		int w = morph_images[i].cols;
+		int h = morph_images[i].rows;
+
+		for (int row = 0; row < h; ++row) {
+			for (int col = 0; col < w; ++col) {
+				int label = labels.at<int>(row, col);
+				if (label == 0) continue;
+				dst.at<cv::Vec3b>(row, col) = colors[label];
+			}
+		}
+
+		for (int j = 1; j < num_labels; ++j) {
+			cv::Vec2d pt = centroids.at<cv::Vec2d>(j, 0);
+			int x = stats.at<int>(j, cv::CC_STAT_LEFT);
+			int y = stats.at<int>(j, cv::CC_STAT_TOP);
+			int width = stats.at<int>(j, cv::CC_STAT_WIDTH);
+			int heigth = stats.at<int>(j, cv::CC_STAT_HEIGHT);
+			int area = stats.at<int>(j, cv::CC_STAT_AREA);
+			if (area >= max_area) {
+				max_area = area;
+				x_max = x;
+				y_max = y;
+				w_max = width;
+				h_max = heigth;
+			}
+		}
+		std::cout << "(x,y) = (" << x_max << ", " << y_max << ") and (width, height) = (" << w_max << ", " << h_max << ")\n";
+		std::cout << "area: " << max_area << "\n";
+		std::cout << "accuracy(square_orig / square_conn_comp): " << sq(coord[i]) / max_area << "\n\n\n";
+		cv::rectangle(dst, cv::Rect(x_max, y_max, w_max, h_max), cv::Scalar(255, 0, 255), 1, 8, 0);
+		std::cout << "\n";
+		conn_comp.push_back(dst);
+		cv::imwrite(name + "con" + std::to_string(i) + ".png", dst);
+	}
+	makeMosaic(conn_comp, "comp_" + name, CV_8UC3);
 }
 
-void connected_component_stats_demo(cv::Mat& image) {
-	cv::Mat gray, binary;
-	cv::cvtColor(image, gray, cv::COLOR_BGR2GRAY);
-	cv::threshold(gray, binary, 0, 255, cv::THRESH_BINARY | cv::THRESH_OTSU);
 
-	cv::Mat k = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3), cv::Point(-1, -1));
-	cv::morphologyEx(binary, binary, cv::MORPH_OPEN, k);
-	cv::morphologyEx(binary, binary, cv::MORPH_CLOSE, k);
-	cv::imshow("binary", binary);
+void makeMosaic(std::vector<cv::Mat> src, std::string name, int type) {
+	cv::Mat res_img(720, 1280, type);
+	src[0].copyTo(res_img(cv::Rect(0, 0, 640, 360)));
+	src[1].copyTo(res_img(cv::Rect(640, 360, 640, 360)));
+	src[2].copyTo(res_img(cv::Rect(0, 360, 640, 360)));
 
-	cv::Mat labels = cv::Mat::zeros(image.size(), CV_32S);
-	cv::Mat stats, centroids;
-	int num_labels = cv::connectedComponentsWithStats(binary, labels, stats, centroids, 8,4);
+	cv::imwrite("lab04_mosaic_" + name + ".png", res_img);
+
+	//return res_img;
+}
+
+double sq(const std::vector<std::pair<int, int> >& fig) {
+	double res = 0;
+	for (unsigned i = 0; i < fig.size(); i++) {
+		std::pair<int, int> p1 = i ? fig[i - 1] : fig.back(),
+			p2 = fig[i];
+		res += (p1.first - p2.first) * (p1.second + p2.second);
+	}
+	return fabs(res) / 2;
+}
+
+int main() {
 	
-	std::cout << "\n\n\n\n\n\n" << num_labels - 1 << "\n\n\n\n\n\n";
-	
-	std::vector<cv::Vec3b> colors(num_labels);
+	std::vector<std::vector<std::pair<int, int> > > p1 = { { {135, 71}, {588, 88}, {586, 283}, {131, 277} },
+																												 { {153, 56}, {589, 74}, {587, 260}, {147, 256} },
+																												 { {99, 58}, {553, 71}, {554, 264}, {96, 265}   } };
+	/*
+	std::vector<std::vector<std::pair<int, int> > > p2 = { { },
+																												 { },
+																												 { } };
 
-	colors[0] = cv::Vec3b(0, 0, 0);
+	std::vector<std::vector<std::pair<int, int> > > p3 = { { },
+																												 { },
+																												 { } };
+
+	std::vector<std::vector<std::pair<int, int> > > p4 = { { },
+																											   { },
+																											   { } };
+
+	std::vector<std::vector<std::pair<int, int> > > p5 = { { },
+																												{ },
+																												 { } };
+*/
 
 
-	for (int i = 1; i < num_labels; ++i) {
-		colors[i] = cv::Vec3b(0, 256, 0);
-	}
 
-	cv::Mat dst = cv::Mat::zeros(image.size(), image.type());
-	int w = image.cols;
-	int h = image.rows;
+	doMagic("../data/rub100(2).mp4", "lab04_rub100_2", p1);
 
-	for (int row = 0; row < h; ++row) {
-		for (int col = 0; col < w; ++col) {
-			int label = labels.at<int>(row, col);
-			if (label == 0) continue;
-			dst.at<cv::Vec3b>(row, col) = colors[label];
-		}
-	}
+	//doMagic("../data/rub200(3).mp4", "lab04_rub200_3", p2);
 
-	for (int i = 1; i < num_labels; ++i) {
-		cv::Vec2d pt = centroids.at<cv::Vec2d>(i, 0);
-		int x = stats.at<int>(i, cv::CC_STAT_LEFT);
-		int y = stats.at<int>(i, cv::CC_STAT_TOP);
-		int width = stats.at<int>(i, cv::CC_STAT_WIDTH);
-		int heigth = stats.at<int>(i, cv::CC_STAT_HEIGHT);
-		int area = stats.at<int>(i, cv::CC_STAT_AREA);
-		std::cout << "area: " << area << "center point " << pt[0] << " " << pt[1];
-		cv::circle(dst, cv::Point(pt[0], pt[1]), 2, cv::Scalar(0, 0, 255), -1, 8, 0);
-		cv::rectangle(dst, cv::Rect(x, y, width, heigth), cv::Scalar(255, 0, 255), 1, 8, 0);
-	}
-	cv::imshow("ccla-demo", dst);
+	//doMagic("../data/rub200(4).mp4", "lab04_rub200_4", p3);
+
+	//doMagic("../data/rub500.mp4", "lab04_rub500", p4);
+
+	//doMagic("../data/rub500(2).mp4", "lab04_rub500_2", p5);
+
+	cv::waitKey(0);
+	return 0;
 }
